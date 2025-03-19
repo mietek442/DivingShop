11536af8d83436c38f5cf747dd16053a7a3e77c2
mietek442 <96135826+mietek442@users.noreply.github.com>
2025-03-19 22:21:58 +0100

    Dodanie Zamówień z obsługą:  kalkulacja ceny  transation - które zabezpiecza żeby dodać zamówienie wraz z jego elementami albo nic

---
+++ Api/Features/Orders/Commands/CreateOrder/OrderItemRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Api.Features.Orders.Commands.CreateOrder
+{
+    public class OrderItemRequest
+    {
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }    
+    }
+}
